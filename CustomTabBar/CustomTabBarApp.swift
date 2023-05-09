dc92e838b504d3f423739928cb801eb6f19c4d85
Adalberto Vergara <adalberto.m.vergara@gmail.com>
2023-05-08 18:06:53 -0600

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Adalberto Vergara on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
